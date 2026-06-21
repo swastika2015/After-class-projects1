201d5250c2b0f1a1ae93f3edac025db777973839
swastika2015 <jmswastika18409@gmail.com>
2026-06-21 12:35:59 +0530

    Add files via upload

---
+++ Power_Calculator.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number whose number you want to find:"))
+power = int(input("Enter the number whose power you want to find:"))
+
+
+result = n ** power
+
+ 
+
+print("answer ",result)
