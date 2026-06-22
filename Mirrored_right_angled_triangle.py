9bb303417ba901bb9785e84082249e48710a4622
swastika2015 <jmswastika18409@gmail.com>
2026-06-22 12:35:30 +0530

    Add files via upload

---
+++ Mirrored_right_angled_triangle.py
@@ -0,0 +1,12 @@
+print("Mirrored right angled triangle(*):")
+n = int(input("Enter the number of rows:"))
+
+for i in range(n):
+    for j in range(n - i - 1 ):
+        print("",end="")
+    for j in range(i + 1):
+        print("*",end="")
+    print()
+
+
+    
