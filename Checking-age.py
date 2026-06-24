a90e185bdb1385bd92cb53ea02c59566895cd355
swastika2015 <jmswastika18409@gmail.com>
2026-06-24 12:15:59 +0530

    Add files via upload

---
+++ Checking-age.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age:"))
+
+if age >= 10 and age  <= 20 :
+    print("You havve qualified the checking age program.....yay..")
+else:
+    print("Sorry you have not qualified the checking age program")
